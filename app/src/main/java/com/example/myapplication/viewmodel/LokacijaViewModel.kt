bcc5d76502f234756056d82184ac35effed8f74d
natsretkovic <natalija.sretkovic@elfak.rs>
2025-08-19 00:38:55 +0200

    bugfix

--- app/src/main/java/com/example/myapplication/viewmodel/LokacijaViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/LokacijaViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.example.myapplication.model.LokacijaKorisnika
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -15,19 +16,26 @@ class LokacijaViewModel() : ViewModel() {
     private val userLoc = MutableStateFlow<LokacijaKorisnika?>(null)
     val userLocation: StateFlow<LokacijaKorisnika?> = userLoc
 
+    private var listenerRegistration: ListenerRegistration? = null
+
     init {
         auth.currentUser?.let { user ->
-            this.userId = user.uid
-            db.collection("korisnici").document(this.userId!!)
+            listenerRegistration = db.collection("korisnici").document(user.uid)
                 .addSnapshotListener { snapshot, error ->
+                    if (error != null) {
+                        return@addSnapshotListener
+                    }
                     if (snapshot != null && snapshot.exists()) {
                         userLoc.value = snapshot.toObject(LokacijaKorisnika::class.java)
+                    } else {
+                        userLoc.value = null // Ako dokument više ne postoji, resetujte vrednost
                     }
-
-
                 }
         }
     }
 
-
+    override fun onCleared() {
+        super.onCleared()
+        listenerRegistration?.remove()
+    }
 }
